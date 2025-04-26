7533ffc8d35fe26ac4394b602cccef829b656d24
erisa1111 <110931465+erisa1111@users.noreply.github.com>
2025-04-26 23:28:51 +0200

    connection with database

--- Config/Database.php
+++ Config/Database.php
@@ -1,24 +1,34 @@
 <?php
-class Database {
+
+namespace Config;
+
+use PDO;
+use PDOException;
+
+class Database
+{
     private $host = "sql7.freesqldatabase.com"; 
     private $db_name = "sql7775438";
     private $username = "sql7775438";
     private $password = "QiwzcdJ5GM";
     public $conn;
 
-    public function getConnection() {
+    public function connect()
+    {
         $this->conn = null;
 
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
-                                  $this->username, 
-                                  $this->password);
-            $this->conn->exec("set names utf8");
-        } catch(PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
+            $this->conn = new PDO(
+                'mysql:host=' . $this->host . ';dbname=' . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // echo "Lidhja me databazen u kry me sukses!";
+        } catch (PDOException $e) {
+            echo 'Lidhja deshtoi: ' . $e->getMessage();
         }
 
         return $this->conn;
     }
 }
-?>
